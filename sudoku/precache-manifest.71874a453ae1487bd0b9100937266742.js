c8b768790ee0c81f5f08d68b1d030fe81af8f191
changzhn <377757708@qq.com>
2019-09-27 16:06:46 +0800

    Site updated: 2019-09-27 16:06:45

---
+++ sudoku/precache-manifest.71874a453ae1487bd0b9100937266742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "695a64cdc365974aba4f9c4a86171762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94980f1677cbf3f52c22",
+    "url": "/static/css/main.1625a751.chunk.css"
+  },
+  {
+    "revision": "6d474037cec2bcf3c764",
+    "url": "/static/js/2.a53017f9.chunk.js"
+  },
+  {
+    "revision": "94980f1677cbf3f52c22",
+    "url": "/static/js/main.af5a74b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e70390ca24baa56f3600d1f841e61725",
+    "url": "/static/media/avatar.e70390ca.jpeg"
+  }
+]);
